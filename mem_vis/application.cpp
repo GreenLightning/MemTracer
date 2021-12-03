f711925a1395fb86c326c5f958227349ff1a9b2b
Green Lightning <GreenLightning.git@googlemail.com>
2021-12-03 17:10:47 +0100

    Add basic instruction analysis

--- mem_vis/application.cpp
+++ mem_vis/application.cpp
@@ -17,10 +17,18 @@
 
 #include "application.hpp"
 
+struct Instruction {
+	uint64_t    addr;
+	const char* opcode;
+	uint64_t    start;
+	uint64_t    count;
+};
+
 struct Trace {
 	std::string filename;
 	mio::mmap_source mmap;
 	header_t header = {};
+	std::vector<Instruction> instructions;
 
 	~Trace() {
 		mmap.unmap();
@@ -69,6 +77,34 @@ struct Trace {
 		int match = MeowHashesAreEqual(actualHash, expectedHash);
 		if (!match) return "invalid file (hash)";
 
+		// TODO: Validate other header fields.
+
+		Instruction* last = nullptr;
+		for (int i = 0; i < header.mem_access_count; i++) {
+			mem_access_t* ma = (mem_access_t*) &mmap[header.mem_access_offset + i * header.mem_access_size];
+			if (ma->cta_id_x != 1 || ma->cta_id_y != 1 || ma->cta_id_z != 0) continue;
+			if (last == nullptr || ma->instr_addr != last->addr) {
+				Instruction instr;
+				instr.addr = ma->instr_addr;
+				instr.opcode = nullptr;
+				instr.start = i;
+				instr.count = 1;
+
+				for (int i = 0; i < header.addr_info_count; i++) {
+					addr_info_t* info = (addr_info_t*) &mmap[header.addr_info_offset + i * header.addr_info_size];
+					if (info->addr == instr.addr) {
+						instr.opcode = (const char*) &mmap[header.opcode_offset + info->opcode_offset];
+						break;
+					}
+				}
+
+				instructions.push_back(instr);
+				last = &instructions[instructions.size()-1];
+			} else {
+				last->count++;
+			}
+		}
+
 		return "";
 	}
 
@@ -76,6 +112,7 @@ struct Trace {
 
 struct Application {
 	int width, height;
+	bool demo = false;
 
 	std::unique_ptr<Trace> trace;
 	std::string status;
@@ -164,16 +201,68 @@ void EndMainStatusBar() {
 }
 
 void appRenderGui(GLFWwindow* window, float delta) {
-	if (app.trace) {
-		ImGui::Text("Filename: %s", app.trace->filename.c_str());
-
-		meow_u128 hash;
-		memcpy(&hash, &app.trace->header.hash, 128 / 8);
-		ImGui::Text("Hash: %08X-%08X-%08X-%08X", MeowU32From(hash, 3), MeowU32From(hash, 2), MeowU32From(hash, 1), MeowU32From(hash, 0));
+	if (ImGui::BeginMainMenuBar()) {
+		if (ImGui::BeginMenu("Help")) {
+			if (ImGui::MenuItem("ImGUI Demo", "", app.demo, true)) {
+				app.demo = !app.demo;
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
 	}
 
 	if (BeginMainStatusBar()) {
 		ImGui::Text(app.status.c_str());
 		EndMainStatusBar();
+	}
+
+	if (ImGui::Begin("Trace")) {
+		if (app.trace) {
+
+			ImGui::Text("Filename: %s", app.trace->filename.c_str());
+
+			meow_u128 hash;
+			memcpy(&hash, &app.trace->header.hash, 128 / 8);
+			ImGui::Text("Hash: %08X-%08X-%08X-%08X", MeowU32From(hash, 3), MeowU32From(hash, 2), MeowU32From(hash, 1), MeowU32From(hash, 0));
+
+			ImGui::Text("Instructions");
+
+			ImGuiTableFlags flags = ImGuiTableFlags_ScrollY | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable;
+
+			if (ImGui::BeginTable("Instructions", 4, flags, ImVec2(0.0f, ImGui::GetContentRegionAvail().y))) {
+				ImGui::TableSetupScrollFreeze(0, 1);
+				ImGui::TableSetupColumn("Index", ImGuiTableColumnFlags_None);
+				ImGui::TableSetupColumn("IP", ImGuiTableColumnFlags_None);
+				ImGui::TableSetupColumn("Opcode", ImGuiTableColumnFlags_None);
+				ImGui::TableSetupColumn("Count", ImGuiTableColumnFlags_None);
+				ImGui::TableHeadersRow();
+
+				ImGuiListClipper clipper;
+				clipper.Begin(app.trace->instructions.size());
+				while (clipper.Step()) {
+					for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; row++) {
+						Instruction& instr = app.trace->instructions[row];
+						ImGui::TableNextRow();
+						ImGui::TableSetColumnIndex(0);
+						ImGui::Text("%d", row);
+						ImGui::TableSetColumnIndex(1);
+						ImGui::Text("0x%016lx", instr.addr);
+						ImGui::TableSetColumnIndex(2);
+						ImGui::Text("%s", instr.opcode);
+						ImGui::TableSetColumnIndex(3);
+						ImGui::Text("%d", instr.count);
+					}
+				}
+				ImGui::EndTable();
+			}
+
+		} else {
+			ImGui::Text("Drag and drop a file over this window to open it.");
+		}
+	}
+	ImGui::End();
+
+	if (app.demo) {
+		ImGui::ShowDemoWindow(&app.demo);
 	}
 }
