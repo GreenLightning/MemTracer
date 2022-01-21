b22e3029779591a1e83fcf6e9253ce48c4a0b64a
Green Lightning <GreenLightning.git@googlemail.com>
2022-01-21 13:25:04 +0100

    Read camera matrix from configuration file

--- rt/config.cc
+++ rt/config.cc
@@ -10,6 +10,14 @@ void loadConfiguration(Configuration& config, const std::string& path) {
 	if (index != std::string::npos) {
 		basePath = path.substr(0, index+1);
 	}
+	#ifdef _WIN32
+	{
+		auto index2 = path.rfind("\\");
+		if (index2 != std::string::npos && (index == std::string::npos || index2 > index)) {
+			basePath = path.substr(0, index2+1);
+		}
+	}
+	#endif
 
 	if (data.contains("input")) {
 		std::string value = toml::find<std::string>(data, "input");
@@ -70,14 +78,26 @@ void loadConfiguration(Configuration& config, const std::string& path) {
 			auto position = toml::find(camera, "position");
 			if (!position.is_array()) throw std::runtime_error(toml::format_error("[error] expected array", position, "here"));
 			if (position.size() != 3) throw std::runtime_error(toml::format_error("[error] position should have three elements", position, "here"));
-			config.camera.x = toml::find<float>(position, 0);
-			config.camera.y = toml::find<float>(position, 1);
-			config.camera.z = toml::find<float>(position, 2);
+			float* pos = &config.camera.x;
+			for (int i = 0; i < 3; i++) {
+				auto value = toml::find(position, i);
+				pos[i] = value.is_floating() ? static_cast<float>(value.as_floating(std::nothrow)) : static_cast<float>(value.as_integer());
+			}
+		}
+
+		if (camera.contains("matrix")) {
+			auto matrix = toml::find(camera, "matrix");
+			if (!matrix.is_array()) throw std::runtime_error(toml::format_error("[error] expected array", matrix, "here"));
+			if (matrix.size() != 9) throw std::runtime_error(toml::format_error("[error] matrix should have 9 elements", matrix, "here"));
+			for (int i = 0; i < 9; i++) {
+				auto value = toml::find(matrix, i);
+				config.camera.mat[i] = value.is_floating() ? static_cast<float>(value.as_floating(std::nothrow)) : static_cast<float>(value.as_integer());
+			}
 		}
 
 		if (camera.contains("vertical_fov")) {
 			auto value = toml::find(camera, "vertical_fov");
-			config.camera.vertical_fov = value.is_floating() ? value.as_floating(std::nothrow) : static_cast<double>(value.as_integer());
+			config.camera.vertical_fov = value.is_floating() ? static_cast<float>(value.as_floating(std::nothrow)) : static_cast<float>(value.as_integer());
 		}
 	}
 
